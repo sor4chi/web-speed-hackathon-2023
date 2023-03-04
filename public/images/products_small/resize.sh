3f1e17494c69187d6eb40da96393e8246c5eae39
sor4chi <sorakawa0218@gmail.com>
2023-03-04 17:15:26 +0900

    fix: 224 * 126でcover crop

---
+++ public/images/products_small/resize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CUR_DIR=`pwd`
+cd $CUR_DIR
+GET_ALL_IMAGES_IN_DEEP=`find . -name "*.jpg"`
+for i in $GET_ALL_IMAGES_IN_DEEP
+do
+    echo "Resize $i"
+    if [ `identify -format "%w" $i` -gt `identify -format "%h" $i` ]; then
+        convert $i -resize x126 -gravity center -crop 224x126+0+0 +repage $i
+    else
+        convert $i -resize 224x -gravity center -crop 224x126+0+0 +repage $i
+    fi
+done
