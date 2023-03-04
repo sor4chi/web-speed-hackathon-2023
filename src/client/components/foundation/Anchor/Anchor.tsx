e061b9310ba5b9d3147438419b8f3cefd82aed93
sor4chi <sorakawa0218@gmail.com>
2023-03-05 00:07:37 +0900

    fix: routerの機能で遷移するように修正

--- src/client/components/foundation/Anchor/Anchor.tsx
+++ src/client/components/foundation/Anchor/Anchor.tsx
@@ -1,11 +1,16 @@
-import type { ComponentProps, FC } from 'react';
+import type { FC, ReactNode } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as styles from './Anchor.styles';
 
-type Props = Omit<ComponentProps<'a'>, 'className'>;
+type Props = {
+  children: ReactNode;
+  href: string;
+  dataTestId?: string;
+};
 
-export const Anchor: FC<Props> = ({ children, href, ...rest }) => (
-  <a className={styles.container()} href={href} {...rest}>
+export const Anchor: FC<Props> = ({ children, dataTestId, href }) => (
+  <Link className={styles.container()} data-testid={dataTestId} to={href}>
     {children}
-  </a>
+  </Link>
 );
