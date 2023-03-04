a5f9197def1c8a6a60ce2fc00e39c4caa570a05b
sor4chi <sorakawa0218@gmail.com>
2023-03-04 19:49:35 +0900

    fix: error boundary戻す

--- src/client/components/application/Providers/Providers.tsx
+++ src/client/components/application/Providers/Providers.tsx
@@ -1,10 +1,10 @@
 import { ApolloProvider } from '@apollo/client';
 import type { FC, ReactNode } from 'react';
-// import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
-// import Fallback from '../../../pages/Fallback';
+import Fallback from '../../../pages/Fallback';
 import { apolloClient } from '../../../utils//apollo_client';
 
 type Props = {
@@ -14,7 +14,9 @@ type Props = {
 export const Providers: FC<Props> = ({ children }) => (
   <ApolloProvider client={apolloClient}>
     <BrowserRouter>
-      <RecoilRoot>{children}</RecoilRoot>
+      <RecoilRoot>
+        <ErrorBoundary FallbackComponent={Fallback}>{children}</ErrorBoundary>
+      </RecoilRoot>
     </BrowserRouter>
   </ApolloProvider>
 );
