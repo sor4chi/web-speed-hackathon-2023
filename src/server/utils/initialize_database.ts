9375d2f4e1bc1f22123b0ac700590220e1972040
sor4chi <sorakawa0218@gmail.com>
2023-03-04 19:03:53 +0900

    feat: productIdへindex

--- src/server/utils/initialize_database.ts
+++ src/server/utils/initialize_database.ts
@@ -1,7 +1,21 @@
 import fs from 'node:fs/promises';
 
+import { Database } from 'sqlite3';
+
 import { DATABASE_PATH, DATABASE_SEED_PATH } from './database_paths';
 
 export const initializeDatabase = async () => {
   await fs.copyFile(DATABASE_SEED_PATH, DATABASE_PATH);
+
+  const db = new Database(DATABASE_PATH);
+
+  await new Promise<void>((resolve, reject) => {
+    db.run('CREATE INDEX index_product_id ON feature_item (productId)', (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
 };
