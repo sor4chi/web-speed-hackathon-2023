9ff470e2dca36bc40d93b72a4173221c277d8c8a
sor4chi <sorakawa0218@gmail.com>
2023-03-05 13:45:49 +0900

    feat: add initialize function

--- public/functions/initialize.js
+++ public/functions/initialize.js
@@ -1,8 +1,8 @@
-export function onRequestPost(context) {
-  const response = context.http.post({
+export const onRequestPost = async (context) => {
+  const response = await context.http.post({
     url: 'https://https://sor4chi-web-speed-hackathon-2023.fly.dev/initialize',
   });
   return new Response(response.body, {
     status: response.status,
   });
-}
+};
