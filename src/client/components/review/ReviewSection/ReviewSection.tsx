1a71d99b1c69ae589af8106273c280a4d6a9e288
sor4chi <sorakawa0218@gmail.com>
2023-03-05 09:41:34 +0900

    refactor: zodを抹消

--- src/client/components/review/ReviewSection/ReviewSection.tsx
+++ src/client/components/review/ReviewSection/ReviewSection.tsx
@@ -2,7 +2,6 @@ import type { FormikErrors } from 'formik';
 import { useFormik } from 'formik';
 import type { FC } from 'react';
 import { memo } from 'react';
-import * as z from 'zod';
 
 import type { ReviewFragmentResponse } from '../../../graphql/fragments';
 import { isEqual } from '../../../utils/object';
@@ -13,8 +12,6 @@ import { ReviewList } from '../ReviewList';
 import * as styles from './ReviewSection.styles';
 
 const LESS_THAN_64_LENGTH_REGEX = /^([\s\S\n]{0,8}){0,8}$/u;
-// NOTE: 改行含めて 64 文字以内であるかどうか確認する
-const commentSchema = z.string().regex(LESS_THAN_64_LENGTH_REGEX);
 
 type Props = {
   reviews: ReviewFragmentResponse[] | undefined;
@@ -37,7 +34,8 @@ export const ReviewSection: FC<Props> = memo(({ hasSignedIn, onSubmitReview, rev
     },
     validate(values) {
       const errors: FormikErrors<ReviewForm> = {};
-      if (values.comment != '' && !commentSchema.safeParse(values.comment).success) {
+      // NOTE: 改行含めて 64 文字以内であるかどうか確認する
+      if (values.comment != '' && !LESS_THAN_64_LENGTH_REGEX.test(values.comment)) {
         errors['comment'] = '64 文字以内でコメントしてください';
       }
       return errors;
