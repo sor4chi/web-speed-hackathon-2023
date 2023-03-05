6b45ee56a502ec18029536188675b9a905960946
sor4chi <sorakawa0218@gmail.com>
2023-03-05 16:42:48 +0900

    feat: preloadできてるかわからん

--- src/client/components/foundation/Image/Image.tsx
+++ src/client/components/foundation/Image/Image.tsx
@@ -5,15 +5,16 @@ import * as styles from './Image.styles';
 
 type Props = Omit<ComponentProps<'img'>, 'className'> & {
   fill?: boolean;
+  eager?: boolean;
 };
 
-export const Image: FC<Props> = ({ fill, ...rest }) => {
+export const Image: FC<Props> = ({ eager, fill, ...rest }) => {
   return (
     <img
       className={classNames(styles.container(), {
         [styles.container__fill()]: fill === true,
       })}
-      loading="eager"
+      loading={eager === true ? 'eager' : 'lazy'}
       {...rest}
     />
   );
--- src/client/components/product/ProductMediaListPreviewer/MediaItemPreviewer/MediaItemPreviewer.tsx
+++ src/client/components/product/ProductMediaListPreviewer/MediaItemPreviewer/MediaItemPreviewer.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react';
 import type { MediaFileFragmentResponse } from '../../../../graphql/fragments';
 import { getMediaType } from '../../../../utils/get_media_type';
 import { DeviceType, GetDeviceType } from '../../../foundation/GetDeviceType';
-import { Image } from '../../../foundation/Image';
+import { Image as FImage } from '../../../foundation/Image';
 
 import * as styles from './MediaItemPreiewer.styles';
 
@@ -15,9 +15,12 @@ type Props = {
 export const MediaItemPreviewer: FC<Props> = ({ file }) => {
   const type = getMediaType(file.filename);
 
+  const img = new Image();
+  img.src = file.filename.replace(/.jpg$/, '.webp');
+
   return (
     <div className={styles.container()}>
-      {type === 'image' && <Image fill src={file.filename.replace(/.jpg$/, '.webp')} />}
+      {type === 'image' && <FImage eager fill src={file.filename.replace(/.jpg$/, '.webp')} />}
       {type === 'video' && (
         <GetDeviceType>
           {({ deviceType }) => (
